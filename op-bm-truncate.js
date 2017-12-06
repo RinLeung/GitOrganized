dfd5bc74780e4c5289d79c5315edc10ae1bc00e3
Rin Leung <rin.leung@protonmail.com>
2017-12-06 12:16:36 -0500

    v2

--- op-bm-truncate.js
+++ op-bm-truncate.js
@@ -9,12 +9,12 @@ var modals = [];
 var count = 0;
 for (i = 0; i < biolength; i++){
 	count = count + 1;
-	newbios += biographies[i].outerHTML.substring(0, 600) + '. . .<p><a href="#op-bm' + count + '" rel="modal:open">Read full bio </a></p></div>'
+	newbios += '<div>' + biographies[i].innerHTML.substring(0, 600) + '<p> . . . <a href="#op-bm' + count + '" rel="modal:open">Read full bio </a></p></div>'
 };
 var count2 = 0;
 for (i = 0; i < biolength; i++){
 	count2 = count2 + 1;
-	modals += '<div id="op-bm'+ count2 +'" class="modal">' + biographies[i].outerHTML + '<a href="#" rel="modal:close">Close</a></div></div>'
+	modals += '<div id="op-bm'+ count2 +'" class="modal">' + biographies[i].innerHTML + '<a href="#" rel="modal:close">Close</a></div></div>'
 };
 
 jQuery(document).ready(function($){
