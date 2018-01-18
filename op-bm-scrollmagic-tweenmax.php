424196cb5eef0065b0c68a5871446e773091bbc3
Rin Leung <rin.leung@protonmail.com>
2018-01-18 18:02:53 -0500

    added front page condition
    added front page condition

--- op-bm-scrollmagic-tweenmax.php
+++ op-bm-scrollmagic-tweenmax.php
@@ -16,18 +16,24 @@ function op_bm_scrollmagic() {
 wp_register_script('scrollmagic', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js','','1.0', true);
 wp_enqueue_script('scrollmagic');
 }
-add_action( 'wp_enqueue_scripts', 'op_bm_scrollmagic' ); 
+
 
 function op_bm_scrollmagic_indicators() {
 wp_register_script('scrollmagic-markers', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js','','1.0', true);
 wp_enqueue_script('scrollmagic-markers');
 }
-add_action( 'wp_enqueue_scripts', 'op_bm_scrollmagic_indicators' ); 
+
 
 function op_bm_gsap_tweenmax() {
 wp_register_script('gsap-tweenmax', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.2/TweenMax.min.js','','1.0', true);
 wp_enqueue_script('gsap-tweenmax');
 }
-add_action( 'wp_enqueue_scripts', 'op_bm_gsap_tweenmax' ); 
+
+
+if (is_front_page()){
+	add_action( 'wp_enqueue_scripts', 'op_bm_scrollmagic' ); 
+	add_action( 'wp_enqueue_scripts', 'op_bm_scrollmagic_indicators' ); 
+	add_action( 'wp_enqueue_scripts', 'op_bm_gsap_tweenmax' ); 
+}
 
 ?>
